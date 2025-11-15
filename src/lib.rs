8a5c2dee32f97fa9d4e05c79c7aa15ebb521a480
joaoviictorti <joaovictorti08@gmail.com>
2025-11-15 14:02:14 -0300

    Small fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 #![no_std]
 #![doc = include_str!("../README.md")]
 #![feature(c_variadic, core_intrinsics)]
-#![allow(
-    clippy::ptr_eq,
-    non_snake_case,
-    non_camel_case_types,
-    internal_features,
-    unsafe_op_in_unsafe_fn
-)]
+#![allow(clippy::function_casts_as_integer)]
+#![allow(clippy::ptr_eq)]
+#![allow(non_snake_case, non_camel_case_types)]
+#![allow(internal_features, unsafe_op_in_unsafe_fn)]
 
 extern crate alloc;
 
